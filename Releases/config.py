163183d3320bd122d14fa3ff70d9fc8c70def28c
Kaenguru-Knobel-Kette <manuel.nowack@gmail.com>
2021-07-31 14:16:44 +0200

    Update config.py

--- Releases/config.py
+++ Releases/config.py
@@ -3,8 +3,8 @@
 DIR_REPO = "C:\\Users\\user\\Documents\\GitHub\\requiem-minor-arcana"
 """Directory where the git repository for Quest Markers Restored is stored."""
 
-DIR_VER = "C:\\Users\\user\\Documents\\Skyrim Tools\\Mod Organizer\\mods\\RMA Release"
+DIR_VER = "C:\\Skyrim Tools\\Mod Organizer\\mods\\RMA Release"
 """Directory where version number is manually added to plugins."""
 
-BSARCH = "C:\\Users\\user\\Documents\\Skyrim Tools\\BSArch\\bsarch.exe"
+BSARCH = "C:\\Skyrim Tools\\BSArch\\bsarch.exe"
 """Path to BSArch.exe"""
